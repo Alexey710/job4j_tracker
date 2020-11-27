1f13ec7c748881b72190b5ed9d54f3eb6c6b06e2
Aleksey Krylov <elsite2015@yandex.ru>
2020-11-27 13:49:00 +0400

    7. Date. Отображение даты. [#411972]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        LocalDateTime date = new Item().getCreated();
+        System.out.println(date.format(formatter));
+    }
+}
