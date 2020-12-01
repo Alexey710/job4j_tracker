e02c9913d90527e7b61a1ee7d68bef1312f0380a
Aleksey Krylov <elsite2015@yandex.ru>
2020-12-01 17:48:02 +0400

    5.1. Тестирование. Подготовка данных. [#411914]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -18,4 +18,48 @@ public class StartUITest {
         Item expected = new Item("Item3");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        Item item2 = new Item("new item2");
+        tracker.add(item);
+        tracker.add(item2);
+        String[] answers = {
+                Integer.toString(item.getId()), "replaced item", Integer.toString(item2.getId()), "replaced item2"
+        };
+        StubInput stubInput = new StubInput(answers);
+        StartUI.editItem(stubInput, tracker);
+        StartUI.editItem(stubInput, tracker);
+
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+        Item replaced1 = tracker.findById(item2.getId());
+        assertThat(replaced1.getName(), is("replaced item2"));
+    }
+
+    @Test
+    public void deleteItem() {
+        Tracker tracker = new Tracker();
+
+        Item item = new Item("new item");
+        Item item2 = new Item("new item2");
+
+        tracker.add(item);
+        tracker.add(item2);
+
+        String[] answers = {
+                Integer.toString(item.getId())};
+
+        StubInput stubInput = new StubInput(answers);
+
+        Item before = tracker.findAll()[0];
+        assertThat(before.getName(), is("new item"));
+
+        StartUI.deleteItem(stubInput, tracker);
+
+        Item after = tracker.findAll()[0];
+        assertThat(after.getName(), is("new item2"));
+    }
 }
