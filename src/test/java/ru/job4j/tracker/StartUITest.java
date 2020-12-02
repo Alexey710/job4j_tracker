c05e279e2e17916f0325ac4acf338b8a035a5974
Aleksey Krylov <elsite2015@yandex.ru>
2020-12-02 15:20:54 +0400

    9.1 Зависимость от System.out [#411903]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,48 +7,20 @@ import static org.junit.Assert.assertThat;
 
 public class StartUITest {
     @Test
-    public void whenCreateItem() {
-        String[] answers = {"0", "Item1", "6"};
-        Input input = new StubInput(answers);
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateItem(), new ShowAllItems(), new EditItem(),
-                new DeleteItem(), new FindItemById(), new FindItemsByName(), new ExitProgram()
+                new ExitProgram(out)
         };
-        new StartUI().init(input, tracker, actions);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Item1");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void whenEditItem0() {
-        String[] answers = {"0", "Item1", "0", "Item2", "2", "1", "replaced Item1", "6"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateItem(), new ShowAllItems(), new EditItem(),
-                new DeleteItem(), new FindItemById(), new FindItemsByName(), new ExitProgram()
-        };
-        new StartUI().init(input, tracker, actions);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("replaced Item1");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        String[] answers = {"0", "Item1", "0", "Item2", "3", "1", "6"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateItem(), new ShowAllItems(), new EditItem(),
-                new DeleteItem(), new FindItemById(), new FindItemsByName(), new ExitProgram()
-        };
-        new StartUI().init(input, tracker, actions);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Item2");
-        assertThat(created.getName(), is(expected.getName()));
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. ===Exit Program===" + System.lineSeparator()
+        ));
     }
 
 }
