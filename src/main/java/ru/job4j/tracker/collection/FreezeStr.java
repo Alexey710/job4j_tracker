8e75e4026a9e689519696a2894f3921a95f5b239
Aleksey Krylov <elsite2015@yandex.ru>
2020-12-19 20:47:37 +0400

    Контрольные вопросы часть 3. Collections. Lite.

--- src/main/java/ru/job4j/tracker/collection/FreezeStr.java
+++ src/main/java/ru/job4j/tracker/collection/FreezeStr.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker.collection;
 
+import java.security.KeyStore;
 import java.util.*;
 
 public class FreezeStr {
@@ -10,6 +11,35 @@ public class FreezeStr {
         Arrays.sort(rightArr);
         for (int i = 0; i < left.length(); i++) {
             if (!leftArr[i].equals(rightArr[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean eq2(String left, String right, String splitter) {
+        String[] leftArr = left.split(splitter);
+        String[] rightArr = right.split(splitter);
+        Map<String, Integer> leftMap = new HashMap<>();
+        Map<String, Integer> rightMap = new HashMap<>();
+        for (String elem : leftArr) {
+           if (leftMap.containsKey(elem)) {
+               leftMap.computeIfPresent(elem, (key, val) -> val + 1);
+           } else {
+               leftMap.put(elem, 1);
+           }
+        }
+
+        for (String elem : rightArr) {
+            if (rightMap.containsKey(elem)) {
+                rightMap.computeIfPresent(elem, (key, val) -> val + 1);
+            } else {
+                rightMap.put(elem, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : leftMap.entrySet()) {
+            if (!rightMap.containsKey(entry.getKey()) || rightMap.get(entry.getKey()) != leftMap.get(entry.getKey())) {
                 return false;
             }
         }
