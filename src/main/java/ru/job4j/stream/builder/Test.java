6a717b0b269e601227a3bea57c4415b33c1966ed
Aleksey Krylov <elsite2015@yandex.ru>
2020-12-22 16:53:27 +0400

    0. Stream API улучшения [#411727]

--- src/main/java/ru/job4j/stream/builder/Test.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.stream.builder;
-
-public class Test {
-    public static void main(String[] args) {
-        Spaceman.Builder obj = new Spaceman.Builder();
-        Spaceman nt = obj.build();
-    }
-}
