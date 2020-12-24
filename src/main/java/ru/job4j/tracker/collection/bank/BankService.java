cc771108b333e4b32d38215d7788e5e97ff0f9a6
Aleksey Krylov <elsite2015@yandex.ru>
2020-12-24 14:54:32 +0400

    3. Что такое JavaDoc [#411871]

--- src/main/java/ru/job4j/tracker/collection/bank/BankService.java
+++ src/main/java/ru/job4j/tracker/collection/bank/BankService.java
@@ -1,20 +1,43 @@
 package ru.job4j.tracker.collection.bank;
 
 import java.util.*;
-
+/**
+ * Класс для работы с банковскими переводами.
+ * Функционал:
+ * -добавлять пользователей {@link User}
+ * -добавлять счета пользователей {@link Account}
+ * -методы поиска по акаунту, по реквизитам
+ *
+ * @author KRYLOV ALEXEY
+ * @version 1.0.
+ * */
 public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * метод добавляет пользователя
+     * @param user
+     */
     public void addUser(User user) {
         List<Account> list = new ArrayList<>();
         users.putIfAbsent(user, list);
     }
 
+    /**
+     * метод добавляет аккаунт пользоввателя
+     * @param passport по номеру паспорта
+     * @param account акаунт {@link Account}, который добавляем
+     */
     public void addAccount(String passport, Account account) {
         Optional<User> target = findByPassport(passport);
         target.ifPresent(user -> users.get(user).add(account));
     }
 
+    /**
+     * метод ищет пользователя по номеру паспорта
+     * @param passport номер паспорта
+     * @return возвращает пользователя {@link User}
+     */
     public Optional<User> findByPassport(String passport) {
         return users.keySet()
                 .stream()
@@ -30,6 +53,12 @@ public class BankService {
         return rsl;*/
     }
 
+    /**
+     * метод находит аккаунт {@link Account} по реквизитам счета и номеру паспорта
+     * @param passport номер паспорта из класса {@link User}
+     * @param requisite ревизиты счета из класса {@link Account}
+     * @return возвращает аккаунт пользователя {@link Account}
+     */
     public Optional<Account> findByRequisite(String passport, String requisite) {
         Optional<User> user = findByPassport(passport);
         if (user.isPresent()) {
@@ -51,6 +80,16 @@ public class BankService {
         return Optional.empty();*/
     }
 
+    /**
+     * метод перемщает деньги с одного аккаунта пользователя {@link Account} на другой аккаунт {@link Account}
+     * по реквизитам счте и паспорту пользователя
+     * @param srcPassport с номера паспорта из класса {@link User}
+     * @param srcRequisite с ревизитов счета из класса {@link Account}
+     * @param destPassport на номер паспорта из класса {@link User}
+     * @param destRequisite на ревизиты счета из класса {@link Account}
+     * @param amount сумма для перевода
+     * @return возвращает true в случае упешного перевода, или в противном случае false
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
